ff4b60f24d406816007b1cc26b4541b927ed0d5b
joy <connect.joypaul@gmail.com>
2025-08-15 20:07:43 +0530

    signup and signin feature added

--- oops_prop.py
+++ oops_prop.py
@@ -14,15 +14,37 @@ class Chartbook:
                             5. Press any other key to exit""")
         
         if user_input =="1":
-            pass
+            self.signup()
         elif user_input == "2":
-            pass
+            self.signin()
         elif user_input == "3":
             pass
         elif user_input == "4":
             pass
         else:
             exit()
+    
+    def signup(self):
+        email = input("enter your email:")
+        pwd = input("enter your password:")
+        self.username = email
+        self.password = pwd
+        print("you have signed up successfully\n")
+        self.menu()
+    def signin(self):
+        if self.username =="" and self.password == "":
+            print("you have not signed up yet press 1 for signin")
+        else:
+            uname = input("enter your username:")
+            pwd = input("enter your password:")
+            if uname == self.username and pwd == self.password:
+                print("sign in successful")
+                self.loggedin = True
+            else:
+                print("Please input correct credictial\n")
+            print("\n")
+            self.menu()
+
 
 obj = Chartbook()
 
